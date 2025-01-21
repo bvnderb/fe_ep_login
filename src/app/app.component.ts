31b1108f8320d8445622e5dc228100c40a92e154
bvnderb <brent.vd.bruggen@gmail.com>
2025-01-21 15:21:32 +0100

    removed a bunch of redundant code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,32 +3,9 @@ import { PopupComponent } from './popup/popup.component';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   imports: [PopupComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  isPopupVisible = false;
-  isLoginFormVisible = true;
-
-  // Method to show the popup and the login form
-  showLoginPopup() {
-    this.isPopupVisible = true;
-    this.isLoginFormVisible = true;
-  }
-
-  // Method to close the popup
-  closePopup() {
-    this.isPopupVisible = false;
-  }
-
-  // Method to switch to the Register form
-  switchToRegisterForm() {
-    this.isLoginFormVisible = false;
-  }
-
-  // Method to switch to the Login form
-  switchToLoginForm() {
-    this.isLoginFormVisible = true;
-  }
-}
+export class AppComponent {}
